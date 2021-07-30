8deff99afe517d21b6eab58488232e2e4c2848dc
Maxime Freychet <max.frecht@gmail.com>
2021-07-30 11:04:30 +0200

    Add RangeFilter by kilometrage, prix, and anneeCirculation to Annonce

--- src/Entity/Annonce.php
+++ src/Entity/Annonce.php
@@ -2,7 +2,9 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Bridge\Doctrine\MongoDbOdm\Filter\RangeFilter;
 use App\Repository\AnnonceRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -12,6 +14,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Entity(repositoryClass=AnnonceRepository::class)
  */
 #[ApiResource]
+#[ApiFilter(RangeFilter::class, properties:['kilometrage', 'anneeCirculation', 'prix'])]
 class Annonce
 {
     /**
